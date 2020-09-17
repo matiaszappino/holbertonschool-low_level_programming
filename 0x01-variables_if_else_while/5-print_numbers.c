f3e5279c40909467fbe60832799388d71a1d4599
Matias Zappino <matiaszappino31@gmail.com>
2020-09-17 18:29:13 +0000

    Ex 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+c = 48;
+while (c <= 57)
+{
+putchar (c);
+c++;
+}
+{
+putchar ('\n');
+}
+return (0);
+}
