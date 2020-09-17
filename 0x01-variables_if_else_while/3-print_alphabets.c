688adc89a8897a2fe950cf4575848c03d1d3bbf0
Matias Zappino <matiaszappino31@gmail.com>
2020-09-17 17:41:02 +0000

    Ex 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+c = 97;
+while (c <= 122)
+{
+putchar (c);
+c++;
+}
+
+
+{
+int c;
+c = 65;
+while (c <= 90)
+{
+putchar (c);
+c++;
+}
+{
+putchar ('\n');
+}
+}
+return (0);
+}
