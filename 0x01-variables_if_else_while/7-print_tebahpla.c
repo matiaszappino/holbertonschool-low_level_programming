b1f616a88910ce1101f9c1af64766fbdfc979af9
Matias Zappino <matiaszappino31@gmail.com>
2020-09-17 18:41:26 +0000

    Ex 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+c = 122;
+while (c >= 97)
+{
+putchar (c);
+c--;
+}
+{
+putchar ('\n');
+}
+return (0);
+}
