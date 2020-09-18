9977c65db4de2d70e5f1427f5f1cdcbf3343e821
Matias Zappino <matiaszappino31@gmail.com>
2020-09-18 17:35:12 +0000

    Ex 11

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,27 +4,26 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int a;
 int b;
-
 for (a = 48; a <= 57; a++)
-{	
-		for (b = 48; b <= 57; b++)
-		{	
-			if (a == b || a > b)
-			{	
-			continue;
-			}
-			putchar (a);
-			putchar (b);
-			if (a != 57 || b != 57)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-		}
+{
+for (b = 48; b <= 57; b++)
+{
+if (a == b || a > b)
+{
+continue;
+}
+putchar (a);
+putchar (b);
+if (a != 57 || b != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
