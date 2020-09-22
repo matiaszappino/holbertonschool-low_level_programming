b71616a83a66a2a957922ab8b3075207a105d748
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-09-22 20:10:09 -0300

    Ex 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,7 @@
 /**
  * print_alphabet - writes the character c to stdout
  *
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: print the alphabet
  */
 void print_alphabet(void)
 {
