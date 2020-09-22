59ae8aefbba3dd6f7e229f9f99ce2ae10990b399
Matias Zappino <matiaszappino31@gmail.com>
2020-09-22 17:59:39 +0000

    Ex 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,10 +10,10 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-while (i < 10) /*This is the line*/
+/*while (i < 10)
 {
-putchar(i); /*This is the line of code causing the loop*/
-}
+putchar(i);
+}*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
