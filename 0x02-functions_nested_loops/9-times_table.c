5625306a2698dfd296814cef68c472d0d46814fe
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-09-22 20:46:08 -0300

    Ex

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,33 @@
-#include <unistd.h>
 #include "holberton.h"
-#include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * times_table - writes the tables to stdout
+ *
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void times_table(void)
-{	
-	int a;
-	int b;
-	for (a = '0'; a <= '9'; a++)
-	{
-		_putchar(a);
-		_putchar('\n');
-
-		for (b = '0'; b <= '9'; b++)
-		{
-		_putchar(b);
-		}
-	}
+{
+int a, b, res;
+for (a = 0; a < 10 ; a++)
+{
+_putchar('0');
+for (b = 1; b < 10 ; b++)
+{
+res = a * b;
+if (res > 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(res / 10 + '0');
+_putchar(res % 10 + '0');
 }
+else
+{
+_putchar(',');
+_putchar (' ');
+_putchar (' ');
+_putchar ('0' + res);
+}}
+_putchar('\n');
+}}
