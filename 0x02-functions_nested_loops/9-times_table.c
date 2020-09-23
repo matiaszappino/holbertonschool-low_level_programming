9efb959c55fc73fc4e1f3155b3679aae291ba8b3
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-09-22 22:16:30 -0300

    Ex 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,29 +2,32 @@
 /**
  * times_table - writes the tables to stdout
  *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void times_table(void)
 {
-int a, b, c;
+int a, b, res;
 for (a = 0; a < 10 ; a++)
 {
 _putchar('0');
 for (b = 1; b < 10 ; b++)
 {
 res = a * b;
-if (c > 9)
+if (res > 9)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(c / 10 + '0');
-_putchar(c % 10 + '0');
+_putchar(res / 10 + '0');
+_putchar(res % 10 + '0');
 }
 else
 {
 _putchar(',');
 _putchar (' ');
 _putchar (' ');
-_putchar ('0' + c);
+_putchar ('0' + res);
 }}
 _putchar('\n');
 }}
