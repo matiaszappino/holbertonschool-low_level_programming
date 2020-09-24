61f0924afd7806a02bde586c8381fb7bc33cd93b
Matias Zappino <matiaszappino31@gmail.com>
2020-09-24 18:26:25 +0000

    Ex 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 
 /**
  * print_most_numbers - writes the character c to stdout
- * 
+ *
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -11,6 +11,7 @@
 void print_most_numbers(void)
 {
 	int a;
+
 	for (a = '0'; a <= '9'; a++)
 	{
 		if (a == '2' || a == '4')
