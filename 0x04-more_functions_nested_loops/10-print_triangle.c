d76a8d9e03bf542e85022f17137ccfff7068a9c4
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-09-24 17:24:47 -0300

    Ex 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,13 @@
+#include <unistd.h>
 
+/**
+ * print_triangle - writes the character c to stdout
+ * @size: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+{
+	print_triangle(int size);
+}
