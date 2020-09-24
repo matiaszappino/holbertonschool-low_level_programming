760f41ea6aa897caf3e96518d6f3645d821eeed6
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-09-24 20:04:41 -0300

    Ex 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,24 +2,21 @@
 /**
  * more_numbers - print
  *
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void more_numbers(void)
 {
-	int a;
-	int b;
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 14; b++)
-		{
-			if (b >= 10)
-			{
-				_putchar(b / 10 + '0');
-			}
-		_putchar(b % 10 + '0');
-		}
-	_putchar('\n');
-	}
+int a;
+int b;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b >= 10)
+{
+_putchar(b / 10 + '0');
+}
+_putchar(b % 10 + '0');
+}
+_putchar('\n');
+}
 }
