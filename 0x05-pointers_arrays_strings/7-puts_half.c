657f428f44004f4c42c0261e6c0b22ccf7e7a92e
Matias Zappino <matiaszappino31@gmail.com>
2020-09-29 15:45:45 +0000

    Ex 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * puts_half - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void puts_half(char *str)
+{
+int c;
+int pm;
+int d;
+
+for (c = 0; *(str + c); c++)
+{
+}
+if ((c % 2) != 0)
+{
+pm = (c + 1) / 2;
+}
+else
+{
+pm = c / 2;
+}
+for (d = pm; d <= c; d++)
+{
+_putchar(*(str + d));
+}
+_putchar('\n');
+}
