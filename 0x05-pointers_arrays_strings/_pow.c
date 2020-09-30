7e69497f04ded3b0dff04455405baeb3217b9f36
Matias Zappino <matiaszappino31@gmail.com>
2020-09-30 19:47:58 +0000

    Ex 10

--- 0x05-pointers_arrays_strings/_pow.c
+++ 0x05-pointers_arrays_strings/_pow.c
@@ -1,12 +1,18 @@
+/**
+ * _pow - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _pow(int n)
 {
-    int xres = 1;
-    int p;
+int xres = 1;
+int p;
 
-    for (p = 0; p < n; p++)
-    {
-        xres *= 10;
-        
-    }
+for (p = 0; p < n; p++)
+{
+xres *= 10;
+}
 return (xres);
 }
