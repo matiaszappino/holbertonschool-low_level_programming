7a8c50cdde54ce567bfc62a3101977f46f398418
Matias Zappino <matiaszappino31@gmail.com>
2020-09-30 20:05:17 +0000

    Ex 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -46,6 +46,12 @@ res += ((s[fin] - '0') * _pow(n)) * signo;
 return (res);
 }
 }
+/**
+ * _pow - function to power of ten
+ * @n: bla bla bla
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _pow(int n)
 {
 int xres = 1;
