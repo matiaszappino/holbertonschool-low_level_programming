e033ca7c27a3c73c12ef72cd8a377718e6c6e302
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-09-30 16:34:03 -0300

    Ex 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+int _atoi(char *s)
+{
+    int a;
+    int res = 0;
+    int signo = 1;
+    int n;
+
+    for (a = 0; s[a] != '\0'; a++)
+    {
+        if(s[a] == '-')
+        {
+            signo *= -1;
+        }
+    
+        if (s[a] >= '0' && s[a] <= '9')
+        {
+            break;
+        }
+    }
+    if (s[a] == '\0')
+    {
+            return(0);
+    }
+    else
+    {
+        
+    int ini = a;
+    int fin;
+
+    for (; s[a] >= '0' && s[a] <= '9'; a++)
+    {
+        fin = a;
+    }
+
+    for (n = 0; fin >= ini; fin--, n++)
+    {
+        res += ((s[fin] - '0') * _pow(n)) * signo;
+    }
+return (res);
+}
+}
