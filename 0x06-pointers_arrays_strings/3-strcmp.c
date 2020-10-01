3eb38ec34f4200e27baed5e18d811ef7c14789a0
Matias Zappino <matiaszappino31@gmail.com>
2020-10-01 15:53:45 +0000

    Ex 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,9 +17,9 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[x] - s2[x]);
 		}
-		else if (s1[x] > s2[x])
+		if (s1[x] > s2[x])
 		{
-			return (s2[x] - s1[x]);
+			return (s1[x] - s2[x]);
 		}
 	x++;
 	}
