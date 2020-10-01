dab08fa15aa8767979cf486b782c09ce4b27b486
Matias Zappino <matiaszappino31@gmail.com>
2020-10-01 18:17:49 +0000

    Ex 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "holberton.h"
 
 /**
  * leet - writes the character c to stdout
@@ -7,20 +7,21 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-char *leet(char *s);
+char *leet(char *s)
 {
-	int i, j;
+	int i;
+	int j;
 
-	letras[11] = {a, A, e, E, o, O, t, T, l, L}
-	numeros[11] = {"4", "4", "3", "3", "0", "0", "7", "7", "1", "1"}
+	char letras[] = "aAeEoOtTlL";
+	char numeros[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; letras[j] != '\0'; j++)
 		{
-			if (s[j] == letras[i])
+			if (s[i] == letras[j])
 			{
-				s[j] = numeros[i];
+				s[i] = numeros[j];
 			}
 		}
 	}
