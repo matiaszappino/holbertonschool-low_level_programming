0b6a241279075aa066f8b9bd1b89be81f6219e17
Matias Zappino <matiaszappino31@gmail.com>
2020-10-01 16:44:38 +0000

    Ex 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/**
+ * string_toupper - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+return (s);
+}
