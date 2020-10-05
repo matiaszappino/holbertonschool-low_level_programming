0b7fa9abb19c27d7528e8068f72a77f4e52334fc
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-05 20:25:29 -0300

    Ex 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,34 +1,28 @@
-#include "include.h"
-
+#include "holberton.h"
 /**
- * _strspn - writes the character c to stdout
- * @s: The character to print
- * @accept: jhaja
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *_strspn - function
+ *@s: 1st string
+ *@accept: st
+ *Return: Bytes
+ *
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	int i, j;
 	int f = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
 	{
-		int j;
-
-		for (j = 0; accept[j] != '\0'; j++)
+		if (s[i] == accept[j])
 		{
-			if (s[i] == accept[j])
-			{
 			f++;
 			break;
-			}
 		}
-	if (!(s[i] != accept[j]))
-	{
+	}
+	if (!(s[i] == accept[j]))
 	break;
-	}
-	}
+}
 return (f);
 }
