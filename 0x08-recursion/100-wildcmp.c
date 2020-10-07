cd94adc4d9574b7805be4286a96454d4a2df8565
Matias Zappino <matiaszappino31@gmail.com>
2020-10-07 20:39:46 +0000

    Ex last

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -7,7 +7,35 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+int asterisco(char *str1, char *str2)
+{
+	if (*s1 == *s2)
+	{
+		return(strcmp(s1, s2));
+	}
+	if (*s1 == '\0')
+	{
+		return(strcmp(str1, str2));
+	}
+
 int wildcmp(char *s1, char *s2)
 {
-	
+	if (*str2 == '*')
+	{
+		return (asterisco(str1, str2 + 1));
+	}
+	if (*str1 == '\0' || str2 == '\0')
+	{
+		if (*str1 == '\0' && *str2 == '\0')
+		{
+			return (1);
+		}
+	return (0);
+	}
+	if (*str1 == *str2)
+	{
+		return (strcmp(str1 + 1, str2 + 1);
+	}
+return (0);
+}
 }
