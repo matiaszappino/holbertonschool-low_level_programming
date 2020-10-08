5ed15492a4eb6b8f53c539d4025ad77a785c194b
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-07 22:40:45 -0300

    Ex 8

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,3 +1,11 @@
+int aux2( char *, char *);
+
+int aux(char *, int, int);
+
+int _putchar(char c);
+
+int auxiliary(int n, int b);
+
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
@@ -15,5 +23,3 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 
 int wildcmp(char *s1, char *s2);
-
-int _putchar(char c);
