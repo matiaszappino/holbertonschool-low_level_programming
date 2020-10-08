df26b5d86054f4f58e9d7125c91f9990b18440b4
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-07 21:57:31 -0300

    Ex 8

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -11,6 +11,7 @@ int wildcmp(char *s1, char *s2)
 		return (aux2(s1, s2));
 	if (*s2 == '\0' && *s1 != '\0')
 		return (0);
+
 	if (*s1 == '\0' || *s2 == '\0')
 	{
 		if (*s1 == '\0' && *s2 == '\0')
@@ -36,12 +37,14 @@ int aux2(char *s1, char *s2)
 	if (*(s2 + 1) == '\0')
 		return (1);
 	s2++;
+
 	if (*s2 + 1 == *s1)
 		return (wildcmp(s1, s2 + 1));
 	else if ((*s2 + 1) == '*')
 		return (aux2(s1, s2 + 1));
 	else if (*(s2 + 1) != '*' && *(s2 + 1) != '\0')
 		return (aux2(s1 + 1, s2));
+
 	if (*s1 == '\0')
 		return (wildcmp(s1, s2));
 	return (aux2(s1 + 1, s2));
