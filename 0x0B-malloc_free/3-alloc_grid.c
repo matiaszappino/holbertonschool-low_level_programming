85bb09debba3073f72fa6b75e879e3d3d6afbba5
Matias Zappino <matiaszappino31@gmail.com>
2020-10-14 19:34:45 +0000

    Ex 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - writes the character c to stdout
+ * @width: The character to print
+ * @height: asda
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, b;
+	int **array;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	array = (int **)malloc(height * sizeof(int *));
+
+	if (array == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(height * sizeof(int));
+	}
+		for (i = 0; i < height; i++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				array[i][b] = 0;
+			}
+		}
+return (array);
+}
