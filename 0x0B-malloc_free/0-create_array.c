3f5c77e547529eef56e0c46a4acd0fea2a3d623b
Matias Zappino <matiaszappino31@gmail.com>
2020-10-14 16:36:56 +0000

    Ex 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - writes the character c to stdout
+ * @size: The character to print
+ * @c: jajska
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+	return ('\0');
+	}
+	p = (char *)malloc(size * sizeof(char));
+
+	for (i = 0; i <= size; i++)
+	{
+	p[i] = c;
+	}
+return (p);
+}
