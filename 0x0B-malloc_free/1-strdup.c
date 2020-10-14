382bef646fda3fb9cb8591ed2fa17070fbf80fc4
Matias Zappino <matiaszappino31@gmail.com>
2020-10-14 16:54:22 +0000

    Ex 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+		}
+
+		p = (char *) malloc(i);
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+		p[i] = str[i];
+		}
+	return (p);
+	}
+return (NULL);
+}
