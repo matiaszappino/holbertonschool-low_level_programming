e230281496390f6c516acfddf12cf450488f11cc
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-15 20:56:17 -0300

    Ex 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - Prints the result of a multiplication
- * @argc: Argument Count
- * @argv: Argument Vector
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - entry point
+ * @argc: count elements
+ * @argv: elements
+ * Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
-	unsigned long int num1, num2, mul;
+	long int num1, num2, mul;
 
 	if (argc != 3)
 	{
@@ -18,15 +18,18 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
 	if (num1 == 0 && *argv[1] != 48)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
+	num2 = atoi(argv[2]);
+	if (num2 == 0 && *argv[2] != 48)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	mul = num1 * num2;
-	printf("%lu", mul);
+	printf("%ld\n", mul);
 	return (0);
 }
