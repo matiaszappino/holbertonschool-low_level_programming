d79f5642be66fe7f9913a8e1cd4dc0228bda0b4e
Matias Zappino <matiaszappino31@gmail.com>
2020-10-15 20:03:10 +0000

    Ex 5

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -10,15 +10,22 @@ int _strlen(char *s);
  */
 int _strlen(char *s)
 {
-        int length;
-        length = 0;
-        while (*s != '\0')
-        {
-                length++;
-                s++;
-        }
-        return (length);
+	int length;
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
+/**
+ * strtow - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 char **strtow(char *str)
 {
         int i, k, palabra, j, m;
@@ -33,7 +40,6 @@ char **strtow(char *str)
 	{
 	return (NULL);
 	}
-
 	for (j = 0; str[j] != '\0'; j++)
 	{
 		if (str[j] == ' ')
@@ -41,32 +47,29 @@ char **strtow(char *str)
 		espacios++;
 		}
 	}
-
-        palabra = 0;
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (str[i] != ' ')
-                {
-                while (str[i] != ' ')
-                {
-                      aux1[k] = str[i];
-                        k++;
-                        i++;
-                }
-              aux1[k++] = '\n';
-                palabra++;
-                }
-        }
+	palabra = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+		while (str[i] != ' ')
+		{
+			aux1[k] = str[i];
+			k++;
+			i++;
+		}
+		aux1[k++] = '\n';
+		palabra++;
+		}
+	}
 	printf("%s\n", aux1);
 	printf("%d\n%d\n", original, espacios);
 	aux2 = (char **) malloc(500);
 
 	if (aux2 == NULL)
-	{
-	return (NULL);
-	}
+		return (NULL);
 	for (m = 0; m <= (original - espacios + palabra); m++)
-{
+	{
 for (n = 0; 
 	{
 		*aux2 = aux1;
