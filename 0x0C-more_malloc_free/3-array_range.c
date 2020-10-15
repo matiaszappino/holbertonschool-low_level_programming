ac8ed36fcf415567acc34caf1f73464c6bedacda
Matias Zappino <matiaszappino31@gmail.com>
2020-10-15 15:57:27 +0000

    Ex 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,9 +10,24 @@
  */
 int *array_range(int min, int max)
 {
+	int *array;
+	int i;
+
 	if (min > max)
 	{
 		return (NULL);
 	}
 
+	array = (int *)malloc(sizeof(int) * max + 1);
 
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; min <= max; i++, min++)
+	{
+	array[i] = min;
+	}
+return (array);
+}
