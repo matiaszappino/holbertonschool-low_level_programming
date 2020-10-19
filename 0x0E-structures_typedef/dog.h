e93e69563492bfd65b32a9d4a9010de57e880b6f
Matias Zappino <matiaszappino31@gmail.com>
2020-10-19 18:23:31 +0000

    Ex 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,3 +18,20 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
+#ifndef dog_n
+#define dog_n
+#define dog_n
+/**
+ * struct dog_t - writes the character c to stdout
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
