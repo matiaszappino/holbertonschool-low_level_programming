9d3f25fc32740e59d701586722d30a0640eb389e
matiaszappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-19 22:59:39 -0300

    Ex 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,29 +1,23 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
+
 /**
- * print_dog - writes the character c to stdout
- * @d: pointer
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_dog - return name of the file asdasgassgasg
+ * @d :asfgasgdshd
+ *
+ * Return: return always 0
  */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
 	if (d->name == NULL)
-		printf("Name: (nil)");
-
+		printf("Name: (nil)\n");
 	else
-	{
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-	}
-
 	if (d->owner == NULL)
-		printf("(nil)");
-
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
