72e70a5112e089c86b2a51632e15976c275f58bf
Matias Zappino <matiaszappino31@gmail.com>
2020-10-21 21:07:18 +0000

    Ex 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,25 +1,32 @@
 #include <stdlib.h>
 #include "3-calc.h"
 #include <stdio.h>
+/**
+ * get_op_func - prints a name as is
+ * @s: name of the person
+ *
+ * Return: Nothing.
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
+
 	i = 0;
 
-	while(ops[i].op)
+	while (ops[i].op)
 	{
-		if(ops[i].op[0] == s[0] && s[1] == '\0')
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
 printf("Error\n");
-exit (99);
-}	
+exit(99);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,17 +14,17 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 
 	res = (*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", res);
 return (0);
-}	
+}
