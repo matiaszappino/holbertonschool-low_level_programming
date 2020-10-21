15da8f31c1e2773ae7e22839c30bb99d0a91d47a
Matias Zappino <matiaszappino31@gmail.com>
2020-10-21 16:26:02 +0000

    Ex 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - function tu print a name
  * @name: the name
@@ -5,6 +6,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL)
+		return;
 
 	f(name);
 }
