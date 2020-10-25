894572abf069aba5da1d9a8ca9bd0e267c2c0ba2
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-25 12:21:38 -0300

    Ex 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * print_name - function tu print a name
- * @name: the name
- * @f: pointer to char
+ *print_numbers - prints numbers
+ *@separator: separator char
+ *@n: arguments number
+ *Return: prints numbers with separator
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list milista;
+	int num;
+	va_list argumentList;
 
-	va_start(milista, n);
+	if (separator == NULL)
+		separator = "";
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%i", va_arg(milista, int));
+	va_start(argumentList, n);
 
-		if (separator == NULL)
+		for (i = 0; i < n; i++)
 		{
-			separator = "";
-		}
-		if (!(i == n - 1))
-		{
+			num = va_arg(argumentList, int);
+			printf("%d", num);
+			if (i == n - 1)
+			{
+				break;
+			}
 			printf("%s", separator);
 		}
-	}
-	printf("\n");
-	va_end(milista);
-return;
+		printf("\n");
+		va_end(argumentList);
 }
