fedbc19bf9c195ffaad1fb9a879108699609c89d
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-25 12:23:16 -0300

    Ex 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * print_strings - function tu print a name
- * @separator: separator
- * @n: number of arguments
+ *print_strings - prints strings
+ *@separator: separator char
+ *@n: arguments number
+ *Return: prints strings with separator
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list milista;
+	va_list argumentList;
 	char *strings;
 
-	va_start(milista, n);
+	va_start(argumentList, n);
+	if (separator == NULL)
+		separator = "";
 
-	for (i = 0; i < n; i++)
-	{
-		strings = va_arg(milista, char*);
-
-		if (separator == NULL)
+		for (i = 0; i < n; i++)
 		{
-			separator = "";
-		}
+			strings = va_arg(argumentList, char *);
 
-		if (strings == NULL)
-		{
-			printf("(nil)");
+			if (strings == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", strings);
+			if (n == i + 1)
+				break;
+		       printf("%s", separator);
 		}
+	printf("\n");
+	va_end(argumentList);
 
-		printf("%s", strings);
-
-		if (!(i == n - 1))
-		{
-			printf("%c ", *separator);
-		}
-	}
-printf("\n");
-va_end(milista);
-return;
 }
