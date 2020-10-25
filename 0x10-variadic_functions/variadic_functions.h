5ae9dfae50b35c9b958de10a48c17411df44d889
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-10-25 12:27:40 -0300

    Ex 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,22 +2,23 @@
 #define VARIADIC_H
 #include <stdarg.h>
 #include <stdio.h>
-/**
- * print_all - function tu print a name
- * @format: the format
- *
- */
-typedef struct miestructura
-{
-        char *op;
-        void (*call)();
-} op_t;
+#include <stdlib.h>
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(char *str, va_list milista);
-void print_int(char *str, va_list milista);
-void print_float(char *str, va_list milista);
-void print_char_pointer(char *str, va_list milista);
+
+/**
+ * struct op - Short description
+ * @c: First member
+ * @func: Second member
+ * Description: Longer description
+ */
+
+typedef struct op
+{
+	char *c;
+	void (*func)();
+} op_t;
 #endif
