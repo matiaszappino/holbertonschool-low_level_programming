d8a5ef5f616103bc8b56b23a6b299106aa135786
Matias Zappino <matiaszappino31@gmail.com>
2020-10-29 19:12:10 +0000

    Ex 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * init_function - function to print a list
+ *
+ * Return: the number of nodes
+ */
+void init_function (void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
