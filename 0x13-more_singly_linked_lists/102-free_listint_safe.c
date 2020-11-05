503cc0d42f559240cf9c2a56b20efd7fe184cfb6
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-04 23:43:37 -0300

    Ex 10

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,17 +1,54 @@
 #include "lists.h"
+#include <stdbool.h>
 /**
- * free_listint_safe - free the list and set head to NULL
- * @h: the inital pointer to the linked list
- * Return: void return
+ *free_listint_safe - free all the elements of a listint
+ *@h: head of list
+ *Return: number of elements
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *aux;
-	size_t i = 0;
+	int number = 0, i;
+	listint_t *aux = NULL, *fast_ptr, *slow_ptr;
+	bool is_loop;
 
-	if (!h)
+	if (h == NULL)
 		return (0);
-	while (*h)
-		aux = *h, *h = (*h)->next, free(aux), i++;
-	return (i);
+	fast_ptr = *h;
+	slow_ptr = *h;
+	is_loop = false;
+
+	while (fast_ptr && slow_ptr && fast_ptr->next)
+	{
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+		number++;
+		if (slow_ptr == fast_ptr)
+		{
+			is_loop = true;
+			break;
+		}
+	}
+	if (is_loop)
+	{
+		fast_ptr = (*h)->next;
+		slow_ptr = *h;
+		for (i = 0; i <= number + 1; i++)
+		{
+			aux = slow_ptr;
+			slow_ptr = slow_ptr->next;
+			fast_ptr = fast_ptr->next;
+			free(slow_ptr);
+		}
+	}
+	if (!is_loop)
+	{
+		while (*h)
+		{
+			aux = *h;
+			*h = (*h)->next;
+			number++;
+			free(aux);
+		}
+	}
+	return (number);
 }
