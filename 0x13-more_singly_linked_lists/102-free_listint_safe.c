3d9640703aac0f1178304616224bf71b27ecb769
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-04 21:09:22 -0300

    Ex 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint_safe - free the list and set head to NULL
+ * @h: the inital pointer to the linked list
+ * Return: void return
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *aux;
+	size_t i = 0;
+
+	if (!h)
+		return (0);
+	while (*h)
+		aux = *h;
+    *h = (*h)->next;
+    free(aux), i++;
+	return (i);
+}
