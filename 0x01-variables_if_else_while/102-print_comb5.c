7d8196803922318286453b86eba070966b98deca
Matias Zappino <matiaszappino31@gmail.com>
2020-11-06 19:18:36 +0000

    Ex 10

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+						if ()
+						{
+							putchar ((a) + '0');
+							putchar ((b) + '0');
+							putchar (' ');
+							putchar ((c) + '0');
+							putchar ((d) + '0');
+						}
+				/**if (c != 9 || d != 9)
+				{
+					putchar (',');
+					putchar (' ');
+				}**/
+				}
+			}
+		}
+	}
+putchar ('\n');
+return (0);
+}
