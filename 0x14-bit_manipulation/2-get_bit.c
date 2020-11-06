55775322cb430e1253d7963355cf25fa6295f0a3
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-05 22:18:06 -0300

    Ex 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,26 +7,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-        unsigned long int len = 1;
-        unsigned long int *pn = &len;
-        if (n == 0 && index > 0)
-                return (-1);
-        len_binary(n, pn);
-        if (index > len - 1)
-                return (-1);
-        if ((n >> index) & 1)
-                return (1);
-        else
-                return (0);
-}
-/**
- *len_binary - lenght of binary number
- *@n: integer to convert
- *@pn: pointer to len
- */
-void len_binary(unsigned long int n, unsigned long int *pn)
-{
-        if (n >> 1)
-                len_binary(n >> 1, pn);
-        *pn += 1;
+
+	if (n == 0 && index > 0)
+		return (-1);
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	if ((n >> index) & 1)
+		return (1);
+	else
+		return (0);
 }
