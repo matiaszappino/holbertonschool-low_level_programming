91f5537e50353cce13b75492333f609bd06aecd4
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-05 22:16:23 -0300

    Ex 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,33 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
- * listint_len - function to return de number of nodes
- * @h: pointer to list
- * Return: the number of nodes
- */
+ *binary_to_uint - convert binary to integer
+*@b:binary number
+*Return: number in decimal base
+*/
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int val = 0;
-  int i = 0;
-  if (b == NULL)
-    return 0;
-  while (b[i] == '0' || b[i] == '1')
-  {
-	val <<= 1;
-	val += b[i]-'0';
-	i++;
-  }
-  return val;
-}
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len;
+
+	if (!b)
+		return (0);
+	while (b[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+	i = 0;
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
+	}
+	return (i);
+	}
