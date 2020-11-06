1dc688102cd6a4a4719a8aab3873b8c182c5326f
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-05 22:19:39 -0300

    Ex 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,10 +5,11 @@
  */
 int get_endianness(void)
 {
-        unsigned int i = 1;
-        char *c = (char*)&i;
-        if (*c)
-                return (1);
-        else
-                return (0);
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
 }
