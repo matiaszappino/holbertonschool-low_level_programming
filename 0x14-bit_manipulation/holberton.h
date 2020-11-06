fa9906d5d528107b45621727d3890743c4097f91
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-05 22:14:27 -0300

    Ex 5

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,9 +1,14 @@
-int _putchar(char c);
+#ifndef HOLBIE_H
+#define HOLBIE_H
+#include <stdio.h>
+#include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
+void len_binary(unsigned long int n, unsigned long int *);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
+#endif
