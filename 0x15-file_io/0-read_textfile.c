789ab6a143c7eaf7bf307eee3f6f60562dd5914b
Matias Zappino <matiaszappino31@gmail.com>
2020-11-09 16:57:54 +0000

    Ex 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open("Requiescat", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 	{
