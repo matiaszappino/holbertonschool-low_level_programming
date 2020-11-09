222a864f7337f6d0c41be7ec45bbc7d2fbd45a56
Matias Zappino <matiaszappino31@gmail.com>
2020-11-09 18:04:35 +0000

    Ex 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,8 +26,8 @@ int create_file(const char *filename, char *text_content)
 	if (fo == -1)
 		return (-1);
 
-	if (i == 1)
-		write(fo, "", 1);
+	if (i == 0)
+		write(fo, "", 0);
 	else
 		write(fo, text_content, i);
 
