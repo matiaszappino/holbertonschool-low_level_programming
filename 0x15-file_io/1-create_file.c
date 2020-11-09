4ac10c40df6b5a71085add862f8461dcdb4bdafb
Matias Zappino <matiaszappino31@gmail.com>
2020-11-09 17:47:13 +0000

    Ex 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,36 @@
-hola
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * create_file - Function tu read a textfile
+ * @filename: Name of the file
+ * @text_content: content
+ * Return: 1 on success -1 on fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fo, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[i] != '\0')
+		i++;
+
+	fo = open(filename, O_CREAT | O_EXCL | O_RDWR, 0600);
+
+	if (fo == -1)
+		return (-1);
+
+	if (i == 0)
+		write(fo, "", 1);
+	else
+		write(fo, text_content, i);
+
+	close(fo);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,1 +1,2 @@
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
