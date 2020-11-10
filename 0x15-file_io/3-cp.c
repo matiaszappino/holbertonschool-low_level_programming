069d364d6f25f5bc828b3c39ae6a383be79a45fd
Matias Zappino <matiaszappino31@gmail.com>
2020-11-10 02:02:18 +0000

    Ex 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,31 @@
 #include "holberton.h"
 /**
+ *ifclosefc1 - copies the content of a file to another file
+ *@fc1: fc1
+ *Return: void
+ */
+void ifclosefc1(int fc1)
+{
+	if (fc1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc1);
+		exit(100);
+	}
+}
+/**
+ *ifclosefc2 - copies the content of a file to another file
+ *@fc2: fc2
+ *Return: void
+ */
+void ifclosefc2(int fc2)
+{
+	if (fc2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc2);
+		exit(100);
+	}
+}
+/**
  *main - copies the content of a file to another file
  *@ac: argument count
  *@av: argument vector
@@ -24,7 +50,6 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	fo2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	
 	while ((numr = read(fo1, buffer, 1024)) > 0)
 	{
 		if (fo2 < 0 || write(fo2, buffer, numr) != numr)
@@ -42,16 +67,8 @@ int main(int ac, char **av)
 		exit(98);
 	}
 	fc1 = close(fo1);
-	if (fc1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc1);
-		exit(100);
-	}
+	ifclosefc1(fc1);
 	fc2 = close(fo2);
-	if (fc2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fc2);
-		exit(100);
-	}
+	ifclosefc2(fc2);
 	return (0);
 }
