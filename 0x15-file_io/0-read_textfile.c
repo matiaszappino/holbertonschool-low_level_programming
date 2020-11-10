96285c8112a7e0bec5572e673f56dd8b66b5fcd8
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-09 22:51:31 -0300

    Ex 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,32 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "holberton.h"
 /**
- * read_textfile - Function tu read a textfile
- * @filename: Name of the file
- * @letters: quantity of letters to read
- * Return: The letters it actually could read and write
+ *read_textfile - reads a text file and prints it to the POSIX std out
+ *@filename: text file
+ *@letters: number of letters should print
+ *Return: number of letters printed
+ *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fo, fr;
 	char *buf;
-	int fr;
-
-	buf = malloc(sizeof(char) * letters);
-
-	if (buf == NULL)
-		return (0);
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-	{
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
-	}
 
-	fr = read(fd, buf, letters);
+	fo = open(filename, O_RDONLY);
+	if (fo == -1)
+		return (0);
+
+	fr = read(fo, buf, letters);
+
 	write(STDOUT_FILENO, buf, fr);
+
 	buf[fr + 1] = '\0';
-
-	close(fd);
-
+	close(fo);
 	return (fr);
 }
