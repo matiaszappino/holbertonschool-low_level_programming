559075807f5205559c326b2a987f3cba619fb160
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-11-09 22:28:20 -0300

    Ex 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,7 +29,7 @@ int main(int ac, char **av)
 	{
 		if (fo2 < 0 || write(fo2, buffer, numr) != numr)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(fo1);
 			exit(99);
 		}
