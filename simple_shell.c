4e98311cbe62dd68d827b6348f485afa2686dc7d
Matias Zappino <matiaszappino31@gmail.com>
2020-11-13 04:19:41 +0000

    Ex 0

--- simple_shell.c
+++ simple_shell.c
@@ -1,62 +1,63 @@
-#include "shell.h"
-size_t prompt (char *pbuffer);
-int read_buffer(char *pbuffer);
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+size_t prompt(char *pbuffer, size_t);
+int read_buffer(char *buffer);
+
 int main(void)
 {
-	int buffersize = 1024;
-	char buffer[1024];
-	char *pbuffer = &buffer;
-	size_t *pbuffersize = &buffersize;
+    size_t buffsize = 1024;
+    char *buffer;
+    char *pbuffer = buffer;
+    pid_t process;
+	int status;
 
-	buffer = malloc(sizeof(char) * buffersize);
-	if (!buffer)
-		return (NULL);
-	while (1)
-	{
-		characters = prompt(pbuffer, pbuffersize);
-		read_buffer(buffer);
-		if (read_buffer)
-		{
-			procer = fork();
-			if (procer == 1)
-				printf("Error\n");
-			if (procer > 0)
-				wait();
-			if (procer == 0)
-				execve(argv[0], NULL, NULL);
-		}
-	}
+    buffer = (char *)malloc(sizeof(char) * buffsize);
+    if (!buffer)
+        return (-1); 
+
+    while (1)
+    {
+        prompt(pbuffer, buffsize);
+        read_buffer(buffer);
+        if (read_buffer)
+        {
+            process = fork();
+            if (process == 1)
+                printf("Error\n");
+            if (process > 0)
+                wait(&status);
+            //if (process == 0)
+        }
+    }
 return (0);
 }
-size_t prompt (char *pbuffer, size_t pbuffersize)
+
+/*imprime prompt en pantalla*/
+size_t prompt(char *pbuffer, size_t buffsize)
 {
-	ssize_t l_characters;
+    ssize_t l_characters;
 
-	l_characters = getline(&pbuffer,&pbuffersize,stdin);
+    write(STDOUT_FILENO, "$ ", 2);
+    getline(&pbuffer, &buffsize, stdin);
 
-	return (l_characters);
+    return (l_characters);
 }
-int read_buffer(char *pbuffer)
-{
-	char *token;
-	int n, i, l = 0;
-	static char **array;
 
-	while (token != NULL)
-	{
-		token = strtok (buffer, " ");
-		n++;
-		array[i] = strtok(buffer, " ");
-		l++;
-		*array = malloc(sizeof(void *) * n);
-		for (i = 0; i > n; i++)
-		{
-			array[i] = malloc(sizeof(char *) * l);
-		}
-	}
-	while (token != NULL)
-	{
-		token = strtok(buffer, " ");
-		*array[i] = token;
-	}
+int read_buffer(char *buffer)
+{
+    char *token;
+	int number_of_tokens= 0;
+
+	token = (char *)strtok(buffer, " ");
+
+    while (token != NULL)
+    {
+        token = (char *)strtok(NULL, " ");
+        number_of_tokens++;
+        printf("%s\n", token);
+    }
 }
