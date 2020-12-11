a854af206f77638518b9a5960a1c31ddc255d602
Matias Zappino <matiaszappino31@gmail.com>
2020-12-11 19:55:00 +0000

    Ex 6

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that returns the nth node of a dlistint_t linked list.
+ * @head: head
+ * @index: index
+ * Return: the node at index or null if the node does not exist
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *aux;
+	unsigned int i = 0;
+
+	if (!head)
+		return (0);
+
+	aux = head;
+
+	while (aux->next)
+	{
+		aux = aux->next;
+		i++;
+		if (i == index)
+		{
+			if (aux == NULL)
+				return (NULL);
+			break;
+		}
+	}
+return (aux);
+}
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - function that returns the sum of all the data (n) of a dlistint_t linked list.
+ * @head: head
+ * Return: if the list is empty, return 0
+ */
 int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *aux;
+	int number = 0;
+
+	aux = head;
+
+	while (aux)
+	{
+		number = number + aux->n;
+		aux = aux->next;
+	}
+return (number);
+}
