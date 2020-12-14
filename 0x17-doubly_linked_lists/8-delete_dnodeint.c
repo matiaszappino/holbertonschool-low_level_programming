742c07885970b30774815ac12b8c05b6947bdc33
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2020-12-14 18:41:26 -0300

    Ex 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,40 +4,22 @@
  * @head: head
  * @index: index
  * Return: Returns 1 if it succeeded, -1 if it failed
- */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
-{
-	dlistint_t *aux, *aux_two = NULL;
-	unsigned int i = 0;
 
 	if (!head)
 		return (-1);
+	if (index >= dlistint_len(*head))
 	if (index >= dlistint_counter(*head))
 		return (-1);
 	aux = *head;
 	if (index == 0)
-	{
-		aux_two = *head;
-		*head = (*head)->next;
-		(*head)->prev = NULL;
 		free(aux_two);
 		return (1);
 	}
+	if (index == dlistint_len(*head) - 1)
 	if (index == dlistint_counter(*head) - 1)
 	{
 		while (i++ < index - 1)
 			aux = aux->next;
-		aux_two = aux->next;
-		aux->next = NULL;
-		free(aux_two);
-		return (1);
-	}
-	while (i++ < index - 1)
-		aux = aux->next;
-	aux_two = aux->next;
-	aux->next = aux->next->next;
-	aux->next->prev = aux;
-	free(aux_two);
 	return (1);
 }
 /**
