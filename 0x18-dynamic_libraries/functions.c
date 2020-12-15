f3e90956dde9630937a969ad5aa48b7af6208966
Matias Zappino <matiaszappino31@gmail.com>
2020-12-15 00:26:20 +0000

    Ex 3

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,50 @@
+/**
+ * add - add a + b
+ * @a: integer a
+ * @b: integer b
+ * Return: returns the result of a + b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - sub a - b
+ * @a: integer a
+ * @b: integer b
+ * Return: returns the result of a - b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - mul a * b
+ * @a: integer a
+ * @b: integer b
+ * Return: returns the result of a * b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - div a / b
+ * @a: integer a
+ * @b: integer b
+ * Return: returns the result of a / b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mod a % b
+ * @a: integer a
+ * @b: integer b
+ * Return: returns the result of a % b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
