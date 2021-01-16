a196c011aaf626c5edb476f5e9810fcaea4beef7
Matias Zappino <65930811+matiaszappino@users.noreply.github.com>
2021-01-15 23:01:45 -0300

    Ex 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,6 +20,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	copy = strdup(value);
 	indice = key_index((const unsigned char *)key, ht->size);
+	for (i = indice; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 	new_element = malloc(sizeof(hash_node_t));
 	if (new_element == NULL)
 		return (0);
