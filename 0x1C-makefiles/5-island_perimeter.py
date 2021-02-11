399b48db27fd629041ec98fcd079a84a57bed9dd
Matias Zappino <matiaszappino31@gmail.com>
2021-02-11 21:35:56 +0000

    Ex 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,14 +2,20 @@
 """Island Perimeter"""
 
 def island_perimeter(grid):
+    """Function that returns the perimeter of an island"""
     counter = 0
-    counter_two = 0
-    for i in grid[0]:
+    height = 0
+    array = []
+    for i in range(len(grid)):
         flag = 0
-        for x in range(len(grid)):
+        counter = 0
+        for x in range(len(grid[0])):
             if grid[i][x] == 1:
                 counter += 1
                 flag = 1
+        if flag == 1 and counter != 0:
+            array.append(counter)
         if flag == 1:
-            counter_two += 1
-    return (counter + counter_two) * 2
+            height += 1
+    width = max(array)
+    return (width + height) * 2
