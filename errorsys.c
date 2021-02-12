a7f11de39ec856dd2210c3a0cf3be46a84b906a1
Matias Zappino <matiaszappino31@gmail.com>
2021-02-12 03:33:15 +0000

    Repository Cleaned

--- errorsys.c
+++
@@ -1,7 +0,0 @@
-/*void errorsys(char *argv, int count, char *error, char *name)
-{
-    write(STDERR_FILENO, name, _strlen(name));
-    write(STDERR_FILENO, ": ", 2);
-    _atoi(count)
-
-}*/
--- prompt.c
+++
@@ -1,9 +0,0 @@
-#include "shell.h"
-/**
- * prompt - Prints our prompt line.
- * Return: Nothing.
- */
-void prompt(void)
-{
-	write(STDOUT_FILENO, "#CisFun$ ", 9);
-}
